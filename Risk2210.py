0b31c890f57bdc89e0fb69dfd646e189c1012e5f
SX64 <gally@ProBook.local>
2014-06-24 19:14:41 -0400

    game concepts

--- Risk2210.py
+++ Risk2210.py
@@ -5,3 +5,27 @@
 #need to copy over what I have with java
 
 #need more cowbell
+
+ideas that need to be implemented: //in no particular order
+space station
+commanders (is a guy)
+borders
+territories
+guys
+commander types
+player
+turn
+turn order
+the moon
+regions, for lack of the correct term
+bonuses (technically boni)
+cards
+card types
+energy
+dice
+an attack
+contiguity
+eight sided dice
+water vs land vs moon territories
+territory ownership
+contiguity
