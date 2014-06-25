7f60642329de6d9acc32a0c9f5b74932738ca4d5
SX64 <gally@ProBook.local>
2014-06-24 22:46:32 -0400

    First working run
    Installed PySide, Installed qt 4.8.6, made file executable. Took
    example code from http://zetcode.com/gui/pysidetutorial/firstprograms/
    as a test.

--- Risk2210.py
+++ Risk2210.py
@@ -6,26 +6,42 @@
 
 #need more cowbell
 
-ideas that need to be implemented: //in no particular order
-space station
-commanders (is a guy)
-borders
-territories
-guys
-commander types
-player
-turn
-turn order
-the moon
-regions, for lack of the correct term
-bonuses (technically boni)
-cards
-card types
-energy
-dice
-an attack
-contiguity
-eight sided dice
-water vs land vs moon territories
-territory ownership
-contiguity
+#ideas that need to be implemented: //in no particular order
+#space station
+#commanders (is a guy)
+#borders
+#territories
+#guys
+#commander types
+#player
+#turn
+#turn order
+#the moon
+#regions, for lack of the correct term
+#bonuses (technically boni)
+#cards
+#card types
+#energy
+#dice
+#an attack
+#contiguity
+#eight sided dice
+#water vs land vs moon territories
+#territory ownership
+#contiguity
+#
+
+
+#pyside test
+
+import sys
+from PySide import QtGui
+
+app = QtGui.QApplication(sys.argv)
+
+wid = QtGui.QWidget()
+wid.resize(250, 150)
+wid.setWindowTitle('Simple')
+wid.show()
+
+sys.exit(app.exec_())
