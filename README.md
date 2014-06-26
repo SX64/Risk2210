751c8e50670387cde6c6ab49339b14f83a8fed30
SX64 <gally@ProBook.local>
2014-06-26 19:54:11 -0400

    GUI improvements
    I am not really sure what is going on

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Risk2210
 Java clone of a modified-rules version of Risk 2210 that I play with my friends
 
 actually, lets try python
+
+using example code from: http://zetcode.com/gui/pysidetutorial/
--- Risk2210.py
+++ Risk2210.py
@@ -4,8 +4,6 @@
 
 #need to copy over what I have with java
 
-#need more cowbell
-
 #ideas that need to be implemented: //in no particular order
 #space station
 #commanders (is a guy)
@@ -30,18 +28,113 @@
 #territory ownership
 #contiguity
 #
+"""
+#OS X 10.9.3
+#using PySide 1.2.1
+#using Qt 4.8.6
+#chmod +x this file, then cd [wherever this is], ./Risk2210.py
+"""
 
 
-#pyside test
+
 
 import sys
-from PySide import QtGui
+import random
+from PySide import *
 
+
+#MacBook panel is 1440x900
+width = 1400
+height = 860
+gfxOff = 40
+
+
+"""
 app = QtGui.QApplication(sys.argv)
 
-wid = QtGui.QWidget()
-wid.resize(250, 150)
-wid.setWindowTitle('Simple')
-wid.show()
+board = QtGui.QWidget()
+board.resize(width, height)
+board.setWindowTitle('earth')
+board.show()
+
+#moon = QtGui.QWidget()
+#moon.resize(250, 150)
+#moon.setWindowTitle('moon')
+# moon.show()
 
 sys.exit(app.exec_())
+
+"""
+class test:
+	def amount():
+		print 'called'
+		
+
+class boardGUI(QtGui.QWidget):
+    
+    def __init__(self):
+        super(boardGUI, self).__init__()
+        
+        self.initUI()
+        
+        
+        
+        
+        
+    def initUI(self):
+    	#QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+        
+        #self.setToolTip('This is a <b>QWidget</b> widget')
+        
+        btn = QtGui.QPushButton('Button\'s spleen', self)
+        #btn.setToolTip('This is a <b>QPushButton</b> widget')
+        foo = test()
+        btn.clicked.connect(foo.amount)
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+    
+    	qbtn = QtGui.QPushButton('Quit', self)
+        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(150, 150)
+    
+    
+        self.setGeometry(gfxOff, gfxOff, width, height)
+        self.setWindowTitle('Risk 2210')
+        #self.setWindowIcon(QtGui.QIcon('web.png'))        this for title bar
+    
+        self.show()
+        
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = boardGUI()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
