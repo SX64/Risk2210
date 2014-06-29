081c16547f14f98a467c2ce5decf19f982ba313e
SX64 <gally@ProBook.local>
2014-06-29 14:47:24 -0400

    Added Map, Changed Button Layout

--- README.md
+++ README.md
@@ -5,4 +5,16 @@ Java clone of a modified-rules version of Risk 2210 that I play with my friends
 
 actually, lets try python
 
-using example code from: http://zetcode.com/gui/pysidetutorial/
+
+Known issues:
+Terminal output: [timestamp] Python[5166:1107] modalSession has been exited prematurely - check for a reentrant call to endModalSession:
+	First appeared after I added name input dialogue box, always printed to terminal after I exit the dialogue. I googled it, I think it is a bug in interfacing between Qt and the OS. does not affect application execution.
+
+
+
+
+
+using example code from:
+	zetcode.com/gui/pysidetutorial/
+	stackoverflow.com/questions/2286864/how-can-i-add-a-picture-to-a-qwidget-in-pyqt4
+	http://www.saltycrane.com/blog/2006/12/larger-python-qt-pyqt-example/ (QSpacerItem)
--- Risk2210.py
+++ Risk2210.py
@@ -8,93 +8,134 @@
 """
 
 
+players = [6,7,-1,-1,-1]
 
+class player:
+	#change all fields to arrays
+	spaceStations = 0
+	cards = 0
+	commanders = 0
+
+class uPlayer:
+	#should be singleton, don't know how to do those
+	currentPlayer = 0
+	
+	def getCurrentPlayer(self): #self needed for button hooked methods of an object
+		print players[self.currentPlayer]
+		
+	def changePlayer(self):
+		self.currentPlayer += 1
+		if self.currentPlayer == len(players) or players[self.currentPlayer] == -1:
+			self.currentPlayer = 0
+			
 
 import sys
+import os
 import random
 from PySide import *
 
 
 #MacBook panel is 1440x900
-width = 700#1400
-height = 430#860
+width = 1400
+height = 860
 gfxOff = 40
 
 
-"""
-app = QtGui.QApplication(sys.argv)
+player = uPlayer() #never make another uPlayer
 
-board = QtGui.QWidget()
-board.resize(width, height)
-board.setWindowTitle('earth')
-board.show()
 
-#moon = QtGui.QWidget()
-#moon.resize(250, 150)
-#moon.setWindowTitle('moon')
-# moon.show()
 
-sys.exit(app.exec_())
 
-"""
 
-def food():
-	print "cheese"
 
-class test:
-	value = 7
-	def __init__(self):
-		value = 8
-	def printme(self):
-		print "it finally works"
-		
 
-foo = test()
 
-class boardGUI(QtGui.QWidget):
+
     
+
+
+
+
+
+
+class boardGUI(QtGui.QWidget): #cannot be QtGui.QMainWindow or button layout fails
+
+
+
+    def assignPlayers(self):
+		text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog', 'Enter your name:')
+		if ok:
+			#print text
+			players = text.split(',') #does not actually assign
+		else:
+			exit()
+	
+	
+	
     def __init__(self):
         super(boardGUI, self).__init__()
         
         self.initUI()
         
-        
-        
+    
+    def buttonSetup(self):
+    	
+    	space = QtGui.QSpacerItem(1,730)
+    	#,QtGui.QSizePolicy.Minimum)#,QtGui.QSizePolicy.Expanding)
+    	
+    	playerbtn = QtGui.QPushButton('Current Player', self)
+        changebtn = QtGui.QPushButton('Change Player', self)
+    	quitbtn = QtGui.QPushButton('Quit', self)
+    	
+    	
+        changebtn.resize(changebtn.sizeHint())
+        quitbtn.resize(quitbtn.sizeHint())
+    	playerbtn.resize(playerbtn.sizeHint())
+    	
+    	buttons = QtGui.QGridLayout()
+    	buttons.setVerticalSpacing(2)
+    	buttons.addItem(space, 0, 0)
+    	buttons.addWidget(playerbtn, 1, 0)
+    	buttons.addWidget(changebtn, 2, 0)
+    	buttons.addWidget(quitbtn, 1, 1)
+    	self.setLayout(buttons)
+    	
+    
+        changebtn.clicked.connect(player.changePlayer)
+        quitbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+    	playerbtn.clicked.connect(player.getCurrentPlayer)
+    
+    
+    	
+    def mapUnderlay(self):
+        earth = QtGui.QLabel(self)
+        earth.setGeometry(0, 0, 1227, 756)
+        #use full ABSOLUTE path to the image, not relative
+        earth.setPixmap(QtGui.QPixmap(os.getcwd() + "/earth.png"))
         
         
     def initUI(self):
+    
+    	#tooltip stuff
     	#QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
-        
         #self.setToolTip('This is a <b>QWidget</b> widget')
-        
-        btn = QtGui.QPushButton('Button\'s spleen', self)
         #btn.setToolTip('This is a <b>QPushButton</b> widget')
-        #foo = test()
-        btn.clicked.connect(food)
-        #btn.clicked.connect(test.printme) #does not work
-        btn.clicked.connect(foo.printme) #does nothing but compiles
-        #btn.clicked.connect(self.absurd)
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)
-    
-    	qbtn = QtGui.QPushButton('Quit', self)
-        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        qbtn.resize(qbtn.sizeHint())
-        qbtn.move(150, 150)
-    
-    
-    	
-    	
+
+        
+        self.buttonSetup()
+        
+                
+        self.mapUnderlay()
+        
+        #self.assignPlayers()
+        
         self.setGeometry(gfxOff, gfxOff, width, height)
         self.setWindowTitle('Risk 2210')
         #self.setWindowIcon(QtGui.QIcon('web.png'))        this for title bar
     
         self.show()
         
-        
-    def absurd(self):
-    	sender = self.sender()
-        self.setWindowTitle('stupidity')
+
         
 def main():
     app = QtGui.QApplication(sys.argv)
