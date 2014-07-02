0794707a03c165fc8a5f93f1cedd21b9775197b0
SX64 <gally@ProBook.local>
2014-07-01 22:45:30 -0400

    Implemented most of the attack code

--- Risk2210.py
+++ Risk2210.py
@@ -9,6 +9,15 @@ from PySide import *
 #using Qt 4.8.6
 #chmod +x this file, then cd [wherever this is], ./Risk2210.py
 """
+
+def rollDice():#attack,defend):
+	a1 = random.randint(1,6)
+	a2 = random.randint(1,6)
+	a3 = random.randint(1,6)
+	d1 = random.randint(1,6)
+	d2 = random.randint(1,6)
+	return [a1,a2,a3,d1,d2]
+
 territoryNames = ["Northwestern Oil Emirate","Alberta","Mexico","American Republic","Exiled States of America","Nunavut","Canada","Republique du Quebec","Continental Biospheres","New Avalon","Iceland GRC","Warsaw Republic","Jotenheim","Imperial Balkania","Ukrayina","Andorra","Argentina","Amazon Desert","Andean Nations","Nuevo Timoto","Zaire Military Zone","Ministry of Djibouti","Egypt","Madagascar","Saharan Empire","Lesotho","Afghanistan","Hong Kong","United Indiastan","Alden","Japan","Pevek","Middle East","Khan Industrial State","Angkhor Wat","Siberia","Enclave of the Bear","Sakha","Australian Testing Ground","Java Cartel","New Guinea","Aboriginal League","Poseidon","Hawaiian Preserve","New Atlantis","Ivory Reef","Neo Paulo","Nova Brasilia","New York","Western Ireland","South Ceylon","Microcorp","Akara","Sung Tzu","Neo Tokyo","Free Afrikaans Republic","Independent Kansas","Shackleton","Southern Anarchist Control Zone"]
 
 territoryBorders = [[1,5,31],[0,5,6,9],[3,8,19],[2,6,7,8],[5,7,10],[0,1,4,6],[1,3,5,8],[3,4,6],[1,2,3,6], [10,11,12,15],[4,9,12],[9,12,13,14,15],[9,10,11,14],[11,14,15,22,32],[11,12,13,26,32,36], [9,11,14,24],[17,18,46,57],[16,18,19,24,47],[16,17,19],[2,17,18],[21,24,25], [20,22,23,24,25,32],[13,21,24,32],[21,25,51],[17,13,15,20,21,22],[20,21,23,45,55], [14,27,28,32,36],[26,28,33,34,35,36,54],[26,27,32,34,50],[31,33,35,37],[31,33,54], [0,29,30,33,37],[14,21,22,26,28],[27,29,30,31,35],[27,28,39],[27,29,33,36,37], [14,26,27,35],[29,31,35],[40,41],[34,40,41,53],[38,39,41],[38,39,40],[8,43],[42,44], [43,54],[25,46],[16,45],[17,48],[47,49],[9,48],[28,51],[23,50,52],[41,51,56],[39,54], [27,30,44,53],[25,56,57,58],[52,55,58],[16,55,58],[55,56,57]]
@@ -16,7 +25,7 @@ territoryBorders = [[1,5,31],[0,5,6,9],[3,8,19],[2,6,7,8],[5,7,10],[0,1,4,6],[1,
 territoryPositions = [[1,2],[3,4],[7,4],[5,7],[0,14],[1,6],[3,7],[3,9],[5,4],[3,17],[1,16],[3,20],[1,20],[4,20],[2,23],[4,18],[16,9],[12,10],[13,9],[10,8],[11,21],[10,23],[7,21],[14,24],[8,18],[14,21],[4,25],[6,32],[7,28],[3,31],[5,36],[1,36],[7,24],[4,32],[8,32],[1,28],[2,26],[1,31],[15,37],[11,33],[11,37],[15,36],[5,0],[7,1],[9,2],[15,17],[17,13],[9,12],[7,11],[4,14],[11,27],[14,27],[15,30],[9,37],[7,38],[22,20],[23,35],[22,7],[23,20]]
 
 territoryLabels = []
-
+attackLists = []
 territories = []
 players = []
 
@@ -40,7 +49,7 @@ class uPlayer:
 		return colors[currentPlayer]
 	
 	def getCurrentPlayer(self): #self needed for button hooked methods of an object
-		print players[self.currentPlayer].name
+		return players[self.currentPlayer].name
 		
 	def changePlayer(self):
 		self.currentPlayer += 1
@@ -85,9 +94,6 @@ class territory:
 
 
 
-
-
-
 #MacBook panel is 1440x900
 width = 1400
 height = 860
@@ -96,7 +102,16 @@ gfxOff = 40
 
 UniversalPlayer = uPlayer() #never make another uPlayer
 
-
+def attack():
+	origin = territories[territoryNames.index(attackLists[0].currentItem().text())]
+	destination = territories[territoryNames.index(attackLists[1].currentItem().text())]
+	if origin.owner != UniversalPlayer.getCurrentPlayer():
+		return
+	if origin.owner == destination.owner:
+		return
+	if origin.armies < 2:
+		return
+	rolls = rollDice()
 
 currentPath = []
 def isContiguous(start, end): #pass in territory objects from territories
@@ -143,7 +158,6 @@ class boardGUI(QtGui.QWidget): #cannot be QtGui.QMainWindow or button layout fai
 	
 		vertSpace = QtGui.QSpacerItem(1,730)
 		horiSpace = QtGui.QSpacerItem(1200,1)
-		#,QtGui.QSizePolicy.Minimum)#,QtGui.QSizePolicy.Expanding)
 		
 		playerbtn = QtGui.QPushButton('Current Player', self)
 		changebtn = QtGui.QPushButton('Change Player', self)
@@ -153,7 +167,7 @@ class boardGUI(QtGui.QWidget): #cannot be QtGui.QMainWindow or button layout fai
 		
 		buttons = QtGui.QGridLayout()
 		buttons.setVerticalSpacing(2)
-		buttons.addItem(vertSpace, 0, 1)			#all these are row,column
+		buttons.addItem(vertSpace, 0, 0)			#all these are row,column
 		buttons.addItem(horiSpace, 1, 0)
 		buttons.addWidget(playerbtn, 1, 0)
 		buttons.addWidget(changebtn, 2, 0)
@@ -179,6 +193,25 @@ class boardGUI(QtGui.QWidget): #cannot be QtGui.QMainWindow or button layout fai
 		buttons.addLayout(territoryMarks,0,0)
 		
 		
+		attackOptions = QtGui.QVBoxLayout()
+		fromT = QtGui.QListWidget()
+		
+		fromT.addItems(territoryNames)
+		toT = QtGui.QListWidget()
+		toT.addItems(territoryNames)
+		
+		global attackLists
+		attackLists.append(fromT)
+		attackLists.append(toT)
+		attackbtn = QtGui.QPushButton('Attack!', self)
+		attackbtn.clicked.connect(attack)
+		attackOptions.addWidget(fromT)
+		attackOptions.addWidget(toT)
+		attackOptions.addWidget(attackbtn)
+		buttons.addLayout(attackOptions,0,1)
+		
+		
+		
 		self.setLayout(buttons)
 		
 	
@@ -196,7 +229,6 @@ class boardGUI(QtGui.QWidget): #cannot be QtGui.QMainWindow or button layout fai
 		earth.setGeometry(0, 0, 1227, 756)
 		#use full ABSOLUTE path to the image, not relative
 		earth.setPixmap(QtGui.QPixmap(os.getcwd() + "/earthw.png"))
-		#earth.setPixmap(QtGui.QPixmap(os.getcwd() + "earth.png"))
 		
 		
 	def initUI(self):
@@ -206,6 +238,7 @@ class boardGUI(QtGui.QWidget): #cannot be QtGui.QMainWindow or button layout fai
 		#self.setToolTip('This is a <b>QWidget</b> widget')
 		#btn.setToolTip('This is a <b>QPushButton</b> widget')
 		
+		
 		self.assignPlayers()
 		
 		self.mapUnderlay()
@@ -213,7 +246,6 @@ class boardGUI(QtGui.QWidget): #cannot be QtGui.QMainWindow or button layout fai
 		self.buttonSetup()
 		
 		self.generateTerritories()
-		
 		
 		
 		
